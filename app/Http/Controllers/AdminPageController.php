986c2c01c6fd00fe03d27be70110aa0bd00fff68
adamssh <adamalamsh@gmail.com>
2026-05-13 00:10:50 +0700

    Create View Structure
    and modifying name too

--- app/Http/Controllers/AdminPageController.php
+++ app/Http/Controllers/AdminPageController.php
@@ -5,13 +5,13 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 
-class AdminDashboardController extends Controller
+class AdminPageController extends Controller
 {   
     public function __construct() {
         $this->middleware('isAdmin');
     }
 
-    public function index()
+    public function dashboard()
     {
         $products = Product::orderBy('nama_produk', 'asc')->get();
         return view('admin-pages.dashboard', compact('products'));
---
+++ app/Http/Controllers/UserPageController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class UserPageController extends Controller
+{
+    public function home()
+    {
+        $recommendations = Product::inRandomOrder()->take(3)->get();
+
+        $catalogs = Product::all();
+
+        return view('home-page', compact(
+            'recommendations',
+            'catalogs'
+        ));
+    }
+}
