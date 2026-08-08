be1fa0b3437d72d5e2efb16d6e7d6fb42451b2dc
galihpraditya <galihpraditya16@gmail.com>
2026-08-09 05:04:02 +0700

    fix: middleware case sensitivity for Linux production environment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'isAdmin' => \App\Http\Middleware\isAdmin::class
+            'isAdmin' => \App\Http\Middleware\IsAdmin::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
